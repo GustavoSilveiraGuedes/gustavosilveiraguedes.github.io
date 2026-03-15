1a2593d54c2b1b7c79c90e78cdba8187ddefe600
Gustavo Silveira <gustavosilveiraguedes@gmail.com>
2026-03-15 18:17:04 -0300

    login com google (não finalizado)

---
+++ alGbra/js/loginRegister.js
@@ -0,0 +1,51 @@
+window.addEventListener("load", function () {
+
+    console.log("Google Login inicializado");
+
+    google.accounts.id.initialize({
+        client_id: "154431325898-rspn81ndile98j638331324b7ugpmkhs.apps.googleusercontent.com",
+        callback: handleCredentialResponse
+    });
+
+    const googleButton = document.getElementById("googleCadastro");
+
+    googleButton.addEventListener("click", function (event) {
+
+        event.preventDefault();
+
+        console.log("Botão Google clicado");
+
+        google.accounts.id.prompt();
+
+    });
+
+});
+
+
+function handleCredentialResponse(response) {
+
+    console.log("Resposta recebida do Google");
+
+    const data = parseJwt(response.credential);
+
+    console.log("Nome:", data.name);
+    console.log("Email:", data.email);
+    console.log("Foto:", data.picture);
+
+}
+
+
+function parseJwt(token) {
+
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+
+    const jsonPayload = decodeURIComponent(
+        atob(base64).split('').map(function (c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join('')
+    );
+
+    return JSON.parse(jsonPayload);
+
+}
