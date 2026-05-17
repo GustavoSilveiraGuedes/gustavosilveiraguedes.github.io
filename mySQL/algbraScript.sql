39995af0c3b9ca10dec256ed54ae9ccdae51ea30
Gustavo Silveira <gustavosilveiraguedes@gmail.com>
2026-05-16 23:06:32 -0300

    insert de algumas fórmulas

--- mySQL/algbraScript.sql
+++ mySQL/algbraScript.sql
@@ -1,27 +1,28 @@
 CREATE DATABASE algbra;
+-- DROP DATABASE algbra;
 USE algbra;
 
 CREATE TABLE usuario(
 	id INT AUTO_INCREMENT,
 	nome VARCHAR(100) NOT NULL,
 	email VARCHAR(255) NOT NULL UNIQUE,
-	senha VARCHAR(255), -- SENHA PODE SER NULA DEVIDO A API DO GOOGLE
+	senha VARCHAR(255), -- SENHA PODE SER NULA DEVIDO A ASI DO GOOGLE
 	PRIMARY KEY (id)
 );
 
 CREATE TABLE formula(
 	id INT AUTO_INCREMENT,
-	nomePrimarioPT VARCHAR(100) NOT NULL,
-    nomeSecundarioPT VARCHAR(100),
-    nomeTercearioPT VARCHAR(100),
-    nomePrimarioEN VARCHAR(100) NOT NULL,
-    nomeSecundarioEN VARCHAR(100),
-    nomeTercearioEN VARCHAR(100),
+	nomePT VARCHAR(100) NOT NULL UNIQUE,
+    aliasPT1 VARCHAR(100),
+    aliasPT2 VARCHAR(100),
+    nomeEN VARCHAR(100) NOT NULL UNIQUE,
+    aliasEN1 VARCHAR(100),
+    aliasEN2 VARCHAR(100),
 	materia VARCHAR(100) NOT NULL,
 	conteudo VARCHAR(100) NOT NULL,
 	numeroVariaveis INT,
 	periodoEnsinado VARCHAR(3) NOT NULL,
-	seculo VARCHAR(8) NOT NULL,
+	seculo VARCHAR(10) NOT NULL,
 	autor VARCHAR(100),
 	PRIMARY KEY (id)
 );
@@ -37,9 +38,36 @@ CREATE TABLE desafioDiario(
 CREATE TABLE participacao(
 	usuario_id INT,
 	desafioDiario_id INT,
-	acertou TINYINT(1) NOT NULL,
+	acertou TINYINT NOT NULL,
 	PRIMARY KEY (usuario_id, desafioDiario_id),
 	FOREIGN KEY (usuario_id) REFERENCES usuario(id),
 	FOREIGN KEY (desafioDiario_id) REFERENCES desafioDiario(id)
 );
--- DROP DATABASE algbra;
+
+INSERT INTO formula(nomePT, aliasPT1, aliasPT2, nomeEN, aliasEN1, aliasEN2, materia, conteudo, numeroVariaveis, periodoEnsinado, seculo, autor)
+													-- FUNÇÕES --
+	VALUES	('Função Primeiro Grau', '', '', 'First Degree Function', '', '', 'Matemática', 'Funções', 4, '9EF', 'XVII', ''),
+    
+			('Fórmula Bhaskara', '', '', 'Quadratic Formula', '', '', 'Matemática', 'Funções', 5, '9EF', 'XVII', ''),
+			('Coordenada x Vértice', '', '', 'Vertex x Coordinate', '', '', 'Matemática', 'Funções', 3, '1EM', 'XVII', ''),
+			
+    
+													-- PROGRESSÕES --
+			('Progressão Aritmética Termo Geral', 'PA Termo Geral', '', 'Arithmetic Sequence General Term', 'AS General Term', '', 'Matemática', 'Prgressões', 4, '1EM', 'XVII', ''),
+			('Progressão Aritmética Razão', 'PA Razão', '', 'Arithmetic Sequence Common Difference', 'AS Common Difference', '', 'Matemática', 'Progressões', 3, '1EM', 'XVII', ''),
+			('Progressão Aritmética Soma Termos', 'PA Soma Termos', '', 'Arithmetic Sequence Sum Terms', 'AS Sum Terms', '', 'Matemática', 'Progressões', 4, '1EM', 'XVII', 'Gauss'),
+			('Progressão Aritmética Número Termos', 'PA Número Termos', '', 'Arithmetic Sequence Number Terms', 'AS Number Terms', '', 'Matemática', 'Progressões', 4, '1EM', 'XVII', ''),
+			
+			('Progressão Geométrica Termo Geral', 'PG Termo Geral', '', 'Geometric Sequence General Term', 'AS General Term', '', 'Matemática', 'Prgressões', 4, '1EM', 'XVII', ''),
+			('Progressão Geométrica Razão', 'PG Razão', '', 'Geometric Sequence Common Difference', 'AS Common Difference', '', 'Matemática', 'Progressões', 3, '1EM', 'XVII', ''),
+			('Progressão Geométrica Soma Termos', 'PG Soma Termos', '', 'Geometric Sequence Sum Terms', 'AS Sum Terms', '', 'Matemática', 'Progressões', 4, '1EM', 'XVII', ''),
+			('Progressão Geométrica Número Termos', 'PG Número Termos', '', 'Geometric Sequence Number Terms', 'AS Number Terms', '', 'Matemática', 'Progressões', 4, '1EM', 'XVII', ''),
+
+													-- GEOMETRIA PLANA --
+			('Soma Ângulos Internos', '', '', 'Internal Angles Sum', '', '', 'Matemática', 'Geometria Plana', 2, '7EF', 'III a.C', 'Euclides'),
+			('Soma Ângulos Externos', '', '', 'Exterior Angles Sum', '', '', 'Matemática', 'Geometria Plana', 0, '7EF', 'III a.C', 'Euclides'),
+			('Número Diagonais', '', '', 'Number Diagonals', '', '', 'Matemática', 'Geometria Plana', 2, '7EF', 'III a.C', 'Euclides'),
+
+			('Área Círculo', '', '', 'Area Circle', '', '', 'Matemática', 'Geometria Plana', 2, '7EF', 'III a.C', 'Arquimedes'),
+			('Área Coroa Circular', '', '', 'Area Annulus', '', '', 'Matemática', 'Geometria Plana', 3, '7EF', 'III a.C', 'Arquimedes'),
+			('Área Segmento Circular', '', '', 'Area Sector', '', '', 'Matemática', 'Geometria Plana', 3, '7EF', 'III a.C', '');
